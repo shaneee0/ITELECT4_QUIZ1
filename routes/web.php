c452eebf52b80fae54ca691b5461319dd82d1980
unknown <shanebalagtas6@gmail.com>
2025-05-23 15:45:41 +0800

    this is fixed index

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,10 @@ Route::middleware('auth')->group(function () {
     
     Route::get('employee', [App\Http\Controllers\employeecontroller::class, 'index']);
     Route::post('employee', [App\Http\Controllers\employeecontroller::class, 'index']);
-    Route::('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+    //Route::('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
     Route::get('employee', [\App\Http\Controllers\employeecontroller::class, 'index']);
-    Route::('employee/create', [App\Http\Controllers\employeecontroller::class, 'create']);
-    Route::('employee', [App\Http\Controllers\employeecontroller::class, 'store']);
+    //Route::('employee/create', [App\Http\Controllers\employeecontroller::class, 'create']);
+    //Route::('employee', [App\Http\Controllers\employeecontroller::class, 'store']);
     // Route::get('employee/{id}/edit', [App\Http\Controllers\employeecontroller::class, 'edit']);
     // Route::put('employee/{id}/edit', [App\Http\Controllers\employeecontroller::class, 'update']);
     // Route::get('employee/{id}/delete', [App\Http\Controllers\employeecontroller::class, 'delete']);
